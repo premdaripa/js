6209b8769a469c4e1a2a6f1310aee0efec6f9429
premdaripa <97514725+premdaripa@users.noreply.github.com>
2023-09-20 18:27:38 +0530

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log( 2 > 1);
+
+//for comparison data type should be same
+
+console.log("2" > 1); //js engine convert string in number
+/* 
+2 >1
+2 >= 1
+2 <= 1
+2 < 1
+2 == "2"
+2 === 2
+*/
