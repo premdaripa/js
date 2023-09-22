614f35e93138527f5f2c2d5928f721302f99b8ee
premdaripa <97514725+premdaripa@users.noreply.github.com>
2023-09-22 16:21:26 +0530

    block and global scope, closure, hoisting, assign function to vars

--- 03_ basics/02_scope.js
+++ 03_ basics/02_scope.js
@@ -10,4 +10,27 @@ if (true) {
 
 // console.log(a) //error scope available only in if
 // console.log(b) //error scope available only in if
-console.log(c) // 30 (Global Scope)
+// console.log(c) // 30 (Global Scope)
+
+//inner function can access the var of outer/parent function its called clouser
+// function one() {
+//     const userName = "Ravi"
+//     function two () {
+//         const website = "Google.com"
+//         console.log(userName)
+//         console.log(website)
+//     }
+//     two()
+// }
+
+// one()
+
+
+// ------------------------------------------------
+
+//add(2,2) not possible because of hoisting
+const add = function(num1=0,num2=0) {
+    return num1 + num2
+}
+
+console.log(add(2,2))
