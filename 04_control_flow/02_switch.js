2338086af64451276d27318a4ae68bb718f8a2fc
premdaripa <97514725+premdaripa@users.noreply.github.com>
2023-09-22 22:36:42 +0530

    switch statement

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,22 @@
+const month = 3
+
+// if break it not present then all code excutaed after case matched 
+switch (month) {
+    case 1:
+        console.log("January") 
+        break;
+
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 2:
+        console.log("April")
+        break;
+
+    default:
+        console.error("Error")
+        break;
+}
