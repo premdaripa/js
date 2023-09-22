73fcf61503de36c2b2a0cbd5714fe5d2e87c9102
premdaripa <97514725+premdaripa@users.noreply.github.com>
2023-09-22 22:55:18 +0530

    truthy and falsy with empty arr and obj check

---
+++ 04_control_flow/03_truthy_falsy_values.js
@@ -0,0 +1,30 @@
+const userEmail = "abc@hmail.com"
+
+if (userEmail) {
+    console.log("Got the email")
+} else {
+    console.log("Email is not present")
+}
+
+// falsy values 
+// false, 0, -0, 0n (bigint), "" (empty string), null, undefiend, NaN
+
+//truthy
+//true, anything not falsy,'0' (zero in str), ' ' (str contain space), [] (empty array), {} (empty obj), function() {} (empty function) 
+
+
+//empty array check 
+/*
+const arr = []
+if (arr.length === 0) consloe.log("Empty array")
+*/
+
+
+//empty obj check 
+
+/*
+const obj = {}
+
+// converting obj keys to array then legth check
+if (Object.keys(obj).length === 0) consloe,log("Empty object")
+*/
