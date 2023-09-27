36fa4517cfa0092d54bb7b93146f8dabd4c5e84e
premdaripa <97514725+premdaripa@users.noreply.github.com>
2023-09-27 16:46:55 +0530

    inhertence with class and obj

--- 10_classes_and_OPPS/inheritence.js
+++ 10_classes_and_OPPS/inheritence.js
@@ -1,26 +1,53 @@
 // inheritance
 
-const User = {
-    name: "xyz",
-    email: "xyz@google.com"
+// const User = {
+//     name: "xyz",
+//     email: "xyz@google.com"
+// }
+
+// const Teacher = {
+//     makeVideo: true
+// }
+
+// const TeachingSupport = {
+//     isAvailable: false
+// }
+
+// const TASupport = {
+//     makeAssignment: 'JS assignment',
+//     fullTime: true,
+//     __proto__: TeachingSupport
+// }
+
+// //old syntax
+// Teacher.__proto__ = User
+
+// // modern syntax
+// Object.setPrototypeOf(TeachingSupport, Teacher)
+
+// inhertence with class
+
+class User {
+    constructor(username) {
+        this.username = username
+    }
+
+    logMe() {
+        console.log(`Username : ${this.username}`)
+    }
 }
 
-const Teacher = {
-    makeVideo: true
+class Teacher extends User {
+    constructor(username, email, password) {
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    teacherlog() {
+        console.log(`Welcome ${this.username}, you're now teacher`)
+    }
 }
 
-const TeachingSupport = {
-    isAvailable: false
-}
-
-const TASupport = {
-    makeAssignment: 'JS assignment',
-    fullTime: true,
-    __proto__: TeachingSupport
-}
-
-//old syntax
-Teacher.__proto__ = User
-
-// modern syntax
-Object.setPrototypeOf(TeachingSupport, Teacher)
+const teacher = new Teacher('Rabi','abc@gmail.com','abc123')
+console.log(teacher)
